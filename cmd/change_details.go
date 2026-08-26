a5b80a1e8b7515272e3e4f345a34eb36aaf63b5b
Ethan Powers <epowers@layerzero.com>
2026-08-26 16:27:13 -0400

    Add Basic Diff Highlighting
    This commit adds VERY basic diff highlighting by detecting if a line
    begins with a '+' or '-' and coloring that line's text accordingly.

--- cmd/change_details.go
+++ cmd/change_details.go
@@ -68,7 +68,16 @@ func (m changeDetailsModel) View(width int, height int) string {
 
 	content := ""
 	if len(m.patch) > 0 {
+		addStyle := lipgloss.NewStyle().Foreground(lipgloss.Green)
+		removeStyle := lipgloss.NewStyle().Foreground(lipgloss.Red)
 		lines := strings.Split(m.patch, "\n")
+		for index, line := range lines {
+			if strings.HasPrefix(line, "+") {
+				lines[index] = addStyle.Render(line)
+			} else if strings.HasPrefix(line, "-") {
+				lines[index] = removeStyle.Render(line)
+			}
+		}
 		start := min(m.cursor, len(lines)-1)
 		end := min(start+height-5, len(lines))
 		content = strings.Join(lines[start:end], "\n")
